1c830b274807161eba30f1e406887562f133edde
N0ThingA11 <sitthinon.meesat@gmail.com>
2019-02-25 17:38:16 +0700

    Add Firebase

--- app/src/main/java/com/sitthinon/jinpaoautomationcontest/MainActivity.java
+++ app/src/main/java/com/sitthinon/jinpaoautomationcontest/MainActivity.java
@@ -3,6 +3,8 @@ package com.sitthinon.jinpaoautomationcontest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.medialablk.easygifview.EasyGifView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,7 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+//        Run Normal Gif Face
+        runNormalGifFace();
+    }
+
+    private void runNormalGifFace() {
+        EasyGifView gifEye = findViewById(R.id.gifEye);
+        EasyGifView gifMouth = findViewById(R.id.gifMouth);
+        gifEye.setGifFromResource(R.drawable.eye);
     }
 
 
