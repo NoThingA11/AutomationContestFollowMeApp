2f716c77e9593d0b09e71e87841730564a767bda
N0ThingA11 <sitthinon.meesat@gmail.com>
2019-02-25 18:00:13 +0700

    Gif Add

--- app/src/main/java/com/sitthinon/jinpaoautomationcontest/MainActivity.java
+++ app/src/main/java/com/sitthinon/jinpaoautomationcontest/MainActivity.java
@@ -2,6 +2,8 @@ package com.sitthinon.jinpaoautomationcontest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.medialablk.easygifview.EasyGifView;
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
 //        Run Normal Gif Face
